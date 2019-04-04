cd918c6a0f972fda3dd8bb0e627ac366008acbab
Cecily <Sanders.Cecily@gmail.com>
2019-04-04 00:41:48 -0400

    Updates

---
+++ precache-manifest.35f9265ba20d98b26a8e4be8481024d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "552f16be417e692a40b8",
+    "url": "/PictureGame/static/js/runtime~main.a8128e26.js"
+  },
+  {
+    "revision": "a65306eb86a31f55b4f4",
+    "url": "/PictureGame/static/js/main.ebc058a4.chunk.js"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/PictureGame/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "a65306eb86a31f55b4f4",
+    "url": "/PictureGame/static/css/main.a1172062.chunk.css"
+  },
+  {
+    "revision": "29778cbfdeb0699692ac651f09b1ce2b",
+    "url": "/PictureGame/index.html"
+  }
+];
